a9eb748969cae4725524b063f82f5b712fbb0940
akash5902 <akashpv592@gmail.com>
2023-02-13 18:14:10 +0530

    message

---
+++ Social/NODE-REST-API/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+mongoose.connect('mongodb://localhost:27017/Social',
+{
+    useNewUrlParser:true //to avoid unwanted warnings
+    
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
